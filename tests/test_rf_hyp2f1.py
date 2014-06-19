cabd43aad049106ee433a8d226c4879f440bca48
Christopher T Small <metasoarous@gmail.com>
2014-06-19 14:04:13 -0700

    Fixed namespacing in rf_hyp2f1 test

--- tests/test_rf_hyp2f1.py
+++ tests/test_rf_hyp2f1.py
@@ -1,21 +1,21 @@
 import unittest
-from beta_rat import h2f1
+from betarat.rf_hyp2f1 import hyp2f1
 from scipy import special
 
 
-class TestH2F1(unittest.TestCase):
+class TestRFHyp2F1(unittest.TestCase):
     """ Just want to make sure that where the scipy solution is actually computable, it aggrees with our exact
     solution; as well as that where the the former is not computable, that we definitely get something
     different."""
     def test_agreement1(self):
         args = [3, -5, 10, 1.3]
-        self.assertAlmostEqual(h2f1(*args), special.hyp2f1(*args))
+        self.assertAlmostEqual(hyp2f1(*args), special.hyp2f1(*args))
 
     def test_agreement2(self):
         args = [5, -8, 20, 0.9]
-        self.assertAlmostEqual(h2f1(*args), special.hyp2f1(*args))
+        self.assertAlmostEqual(hyp2f1(*args), special.hyp2f1(*args))
 
     def test_disagreement(self):
         args = [40, -200, 250, 1.3]
-        self.assertNotAlmostEqual(h2f1(*args), special.hyp2f1(*args))
+        self.assertNotAlmostEqual(hyp2f1(*args), special.hyp2f1(*args))
 
